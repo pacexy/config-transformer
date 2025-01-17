2a46645c22f23a14842aa0e40a4ce954effdf132
pacexy <pacexy@gmail.com>
2025-01-17 18:29:21 +0800

    refactor: update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import path from 'node:path/posix'
+import { isFile } from './utils.js'
+
 /**
  * @typedef {{ rules: Rules }} Config
  * @typedef {{ [x: string]: any }} Rules
  */
-
-import path from 'node:path/posix'
 
 /**
  * @param {Rules} rules
@@ -13,14 +14,15 @@ import path from 'node:path/posix'
 export function transform(rules, baseUrl = '', files = {}) {
   for (const [file, value] of Object.entries(rules)) {
     const url = path.join(baseUrl, file)
-    if (typeof value === 'object') {
-      if (value.__content__) {
-        files[url] = value.__content__
-      } else {
-        transform(value, url, files)
-      }
-    } else {
+
+    if (typeof value !== 'object') {
       throw new TypeError('Invalid rule')
+    }
+
+    if (isFile(value)) {
+      files[url] = value.__content__
+    } else {
+      transform(value, url, files)
     }
   }
 
