70a30f1f836edb74b87a8590779cfee63ef4b775
pacexy <pacexy@gmail.com>
2025-01-17 18:26:07 +0800

    feat: generate file to content map

--- src/index.js
+++ src/index.js
@@ -3,16 +3,22 @@
  * @typedef {{ [x: string]: any }} Rules
  */
 
+import path from 'node:path/posix'
+
 /**
  * @param {Rules} rules
+ * @param {string} baseUrl
+ * @param {{ [x: string]: any }} files
  */
-export function transform(rules) {
-  /** @type {{ [x: string]: any }} */
-  const files = {}
-
+export function transform(rules, baseUrl = '', files = {}) {
   for (const [file, value] of Object.entries(rules)) {
+    const url = path.join(baseUrl, file)
     if (typeof value === 'object') {
-      files[file] = value.__content__ ?? transform(value)
+      if (value.__content__) {
+        files[url] = value.__content__
+      } else {
+        transform(value, url, files)
+      }
     } else {
       throw new TypeError('Invalid rule')
     }
